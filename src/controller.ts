379c2616fac4409398ee5927c5aef14524dde17c
Michael Allen Elguira <AllenElguira16@gmail.com>
2021-10-26 00:03:50 +0800

    Feature Express Controller and Response Error created

--- src/controller.ts
+++ src/controller.ts
@@ -1,7 +1,9 @@
-import { Express } from "express";
+import Express from "express";
 import path from "path";
 import fs from 'fs';
 import ResponseError from "./response-error";
+
+const router = Express.Router();
 
 /**
  * Registers Controller to Express Router
@@ -10,52 +12,47 @@ import ResponseError from "./response-error";
  * 
  * @author Michael Allen Elguira <michael01@simplexi.com.ph>
  */
-const registerController = (app: Express, controllerDirectory: string) => {
-  return new Promise<void>((resolve, reject) => {
-    try {
-      const controllerFilenames = getFilenames(controllerDirectory);
+const registerController = (controllerDirectory: string) => {
+  const controllerFilenames = getFilenames(controllerDirectory);
 
-      controllerFilenames.forEach((controllerFilename: string) => {
-        const [filename] = controllerFilename.split('.');
-        const handler = require(path.join(controllerDirectory, controllerFilename));
+  controllerFilenames.forEach((controllerFilename: string) => {
+    const [filename] = controllerFilename.split('.');
+    const handler = require(path.join(controllerDirectory, controllerFilename));
 
-        const uri = '/' + filename
-          .replace(/\[([\w\-. ])+\]/gm, (sMatchedString) => ':' + sMatchedString.slice(1, -1))
-          .replace(/\\/, '\/')
-          .replace('/index', '');
+    const uri = '/' + filename
+      .replace(/\[([\w\-. ])+\]/gm, (sMatchedString) => ':' + sMatchedString.slice(1, -1))
+      .replace(/\\/, '\/')
+      .replace('/index', '');
 
-        Object.keys(handler).forEach((method: string) => {
-          type TExpressMethods = 'get'|'post'|'put'|'delete';
-          
-          app[method.toLocaleLowerCase() as TExpressMethods](uri, async (...oArgs) => {
-            const [request, response] = oArgs;
-            try {
-              await handler[method](...oArgs);
-            } catch (error) {
-              if (error instanceof ResponseError) {
-                response.status(error.code).json({
-                  success: false,
-                  status: error.code,
-                  message: error.message
-                });
-              } else {
-                response.status(500).json({
-                  success: false,
-                  status: 500,
-                  content: {
-                    ...error
-                  }
-                });
+    Object.keys(handler).forEach((method: string) => {
+      type TExpressMethods = 'get'|'post'|'put'|'delete';
+      
+      router[method.toLocaleLowerCase() as TExpressMethods](uri, async (...oArgs) => {
+        const [request, response] = oArgs;
+        try {
+          await handler[method](...oArgs);
+        } catch (error) {
+          if (error instanceof ResponseError) {
+            response.status(error.code).json({
+              success: false,
+              status: error.code,
+              message: error.message
+            });
+          } else {
+            response.status(500).json({
+              success: false,
+              status: 500,
+              content: {
+                ...error
               }
-            }
-          });
-        });
+            });
+          }
+        }
       });
-      resolve();
-    } catch (err) {
-      reject(err);
-    }
+    });
   });
+
+  return router;
 }
 
 /**
